54a5fb45b6907357ca629548f2cd089d683339eb
Sean Hong <iamseanhong@gmail.com>
2018-05-06 13:03:11 -0700

    Check Input exsits

--- LootCheck/src/js/components/Wallet.js
+++ LootCheck/src/js/components/Wallet.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import * as Styled from './Wallet.styles';
 
+export const Input = styled.input`
+  &:focus {
+    outline: none;
+  }
+`;
+
 export class Wallet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+    };
+  }
+
   render() {
     return (
       <div>
         <div className="wallet">{this.props.balance}</div>
+        <br />
+        <Input value={this.state.userInput} onChange={e => this.setState({ userInput: e.target.value })} />
       </div>
     );
   }
--- LootCheck/src/js/components/Wallet.test.js
+++ LootCheck/src/js/components/Wallet.test.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { Wallet } from './Wallet'; // kind of weird has to be this way ...
 // import Wallet from './Wallet';
 
+import { Input } from './Wallet';
+
 import * as Styled from './Wallet.styles';
 
 describe('Wallet Component', () => {
@@ -17,4 +19,8 @@ describe('Wallet Component', () => {
   test('show correct balance', () => {
     expect(wallet.find('.wallet').text()).toEqual('300');
   });
+
+  test('check input exsit', () => {
+    expect(wallet.find(Input).exists()).toBe(true);
+  })
 });
